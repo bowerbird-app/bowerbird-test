0cd2511874bb761d8beacb1112acbd78ad6b20f6
Malcolm Jeremiah Rajeli <maljer@fortnynja.com>
2022-02-27 16:07:54 +0800

    Dynamic root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  
   devise_for :users
   resources :images
   resources :tags
 
-  root 'images#index'
-  
+  unauthenticated do
+    devise_scope :user do
+      root "devise/sessions#new"
+    end
+  end
+
+  authenticated :user do
+    root 'images#index', as: :authenticated_root
+  end
 end
