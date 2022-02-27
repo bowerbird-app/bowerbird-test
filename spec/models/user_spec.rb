6e7d7d8111adfdd55f2663d38e23b8385ac72374
minipin92 <100469700+minipin92@users.noreply.github.com>
2022-02-27 15:04:31 +0800

    added user factory and user model test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'validation test' do
+    it 'ensure email presence' do
+      user = User.new attributes_for(:user, email: nil)
+      expect(user.save).to eq(false)
+    end
+
+    it 'ensure password presence' do
+      user = User.new attributes_for(:user, password: nil)
+      expect(user.save).to eq(false)
+    end
+
+    it 'ensure email format valid' do
+      user = User.new attributes_for(:user, email: 'not_an_email_address.com')
+      expect(user.save).to eq(false)
+    end
+
+    it 'ensure password more than 6 characters' do
+      user = User.new attributes_for(:user, password: '12345')
+      expect(user.save).to eq(false)
+    end
+  end
+end
