0e7d4c863d166e63e800176cec67366b8348b3bb
minipin92 <100469700+minipin92@users.noreply.github.com>
2022-02-28 23:47:51 +0800

    added size column to image and image file & name validation

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,7 +4,15 @@ class Image < ApplicationRecord
   has_many :image_tags, dependent: :destroy
   has_many :tags, through: :image_tags
 
+  validates :name, :file, presence: true
+
+  before_save :set_file_size, if: -> { self.size.nil? }
+
   scope :filter_by_tag, ->(tag_id) { joins(:image_tags).where(image_tags: { tag_id: tag_id }) }
   scope :query_by_name, ->(name)   { where("name ILIKE ?", "%#{name}%") }
 
+  private
+    def set_file_size
+      self.size = self.file.size
+    end
 end
