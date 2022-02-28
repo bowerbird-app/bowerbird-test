d45cc95493c533e20bb542fa2396bb729b88fbd2
minipin92 <100469700+minipin92@users.noreply.github.com>
2022-02-28 23:45:30 +0800

    added images query params and named scope

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,10 @@
 class Image < ApplicationRecord
   mount_uploader :file, ImageUploader
 
-  has_many :image_tags
+  has_many :image_tags, dependent: :destroy
   has_many :tags, through: :image_tags
+
+  scope :filter_by_tag, ->(tag_id) { joins(:image_tags).where(image_tags: { tag_id: tag_id }) }
+  scope :query_by_name, ->(name)   { where("name ILIKE ?", "%#{name}%") }
+
 end
