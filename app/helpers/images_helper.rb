4b83d1f7454f15df27499a5ac99d9400defcbafb
minipin92 <100469700+minipin92@users.noreply.github.com>
2022-03-01 12:26:43 +0800

    added images index & show pages with styling and added helpers

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,2 +1,30 @@
 module ImagesHelper
+  def current_selected_tag_id
+    if params[:tag_id].present?
+      params[:tag_id].to_i
+    else
+      nil
+    end
+  end
+
+  def current_sort_by
+    if params[:sort_by].present? and params[:sort_by] == 'desc'
+      'desc'
+    else
+      'asc'
+    end
+  end
+
+  def reverse_sort_by
+    if params[:sort_by].present? and params[:sort_by] == 'desc'
+      'asc'
+    else
+      'desc'
+    end
+  end
+
+  def query_params
+    # we only want the query params with value
+    params.permit(:tag_id, :name, :sort_by).select { |k, v| v.present? }
+  end
 end
