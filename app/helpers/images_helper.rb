fe0f1db4bfcd530b92705df51884de0b9618f794
minipin92 <100469700+minipin92@users.noreply.github.com>
2022-03-01 15:06:21 +0800

    refractor image table and search bar into shared components

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -7,24 +7,8 @@ module ImagesHelper
     end
   end
 
-  def current_sort_by
-    if params[:sort_by].present? and params[:sort_by] == 'desc'
-      'desc'
-    else
-      'asc'
-    end
-  end
-
-  def reverse_sort_by
-    if params[:sort_by].present? and params[:sort_by] == 'desc'
-      'asc'
-    else
-      'desc'
-    end
-  end
-
   def query_params
     # we only want the query params with value
-    params.permit(:tag_id, :name, :sort_by).select { |k, v| v.present? }
+    params.slice(:tag_id, :name, :sort_by).select { |k, v| v.present? }
   end
 end
