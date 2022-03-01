67f42341071fa65e2f02eda0889f3760d3701d20
minipin92 <100469700+minipin92@users.noreply.github.com>
2022-03-01 15:08:43 +0800

    refractor sorting helpers to shared helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def current_sort_by
+    if params[:sort_by].present? and params[:sort_by] == 'desc'
+      'desc'
+    else
+      'asc'
+    end
+  end
+
+  def reverse_sort_by
+    if params[:sort_by].present? and params[:sort_by] == 'desc'
+      'asc'
+    else
+      'desc'
+    end
+  end
 end
