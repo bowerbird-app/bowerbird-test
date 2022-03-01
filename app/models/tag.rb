f467ba524b3588e6718c682b9b1b42f48accd729
minipin92 <100469700+minipin92@users.noreply.github.com>
2022-03-01 14:48:44 +0800

    updated tag model and factory, added tag model test

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ApplicationRecord
   has_many :image_tags
   has_many :images, through: :image_tags
+
+  validates :name, presence: true, uniqueness: true
+
+  scope :query_by_name, ->(name)   { where("name ILIKE ?", "%#{name}%") }
 end
