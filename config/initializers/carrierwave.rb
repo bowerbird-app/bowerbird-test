4d69067ef19ce772b787bc870ba6e118d5f7100a
Malcolm Jeremiah Rajeli <maljer@fortnynja.com>
2022-03-02 09:24:32 +0800

    Added production credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,14 @@ if !Rails.env.test?
     aws_credentials = Rails.application.credentials.dig(Rails.env.to_sym, :aws)
 
     config.storage = :aws
-    config.aws_bucket = aws_credentials&.dig(:bucket_name)
+    config.aws_bucket = aws_credentials.dig(:bucket_name)
     config.aws_acl = 'private'
     # config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
     config.aws_credentials = {
-      access_key_id: aws_credentials&.dig(:aws_access_key_id),
-      secret_access_key: aws_credentials&.dig(:aws_secret_access_key),
-      region: aws_credentials&.dig(:region),
+      access_key_id: aws_credentials.dig(:aws_access_key_id),
+      secret_access_key: aws_credentials.dig(:aws_secret_access_key),
+      region: aws_credentials.dig(:region),
       stub_responses: Rails.env.test?
     }
 
