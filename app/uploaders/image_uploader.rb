7eff617eef77667d6ff66348431a4f8be84131b3
minipin92 <100469700+minipin92@users.noreply.github.com>
2022-03-07 22:42:23 +0800

    added thumbnail processing to the photo and display thumbnail in image table

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # we configure this in config/initializers/carrierwave.rb
@@ -31,9 +31,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fill: [75, 75]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
